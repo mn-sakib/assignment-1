6da0a6b4175b686de77080d6107c3660f4a2295c
Md Najmus Sakib <mnsakib.me@gmail.com>
2024-01-09 14:57:38 +0600

    modify the age verifier

--- js/main.js
+++ js/main.js
@@ -2,20 +2,13 @@
 let ageVerifier = document.getElementById("verifyButton");
 // Age Verifier 
 ageVerifier.addEventListener("click", function(){
-    let getValue = document.getElementById("ageInput").value;
+    let getValue = parseInt(document.getElementById("ageInput").value);
     let showResult = document.getElementById("ageResult");
-    // Use for color removal
-    showResult.classList.remove("text-danger");
-
     if(getValue >= 18){
         showResult.innerText = "You are an Adult";
     }else if(getValue > 0){
         showResult.innerText = "You are a Child";
-    }else if(getValue == ""){
-        showResult.innerText = "You have not entered any value!";
-        showResult.classList.add("text-danger");
     }else{
-        showResult.innerText = "You've entered a wrong Age!";
-        showResult.classList.add("text-danger");
+        showResult.innerText = "You've entered a wrong age!";
     }
 });
